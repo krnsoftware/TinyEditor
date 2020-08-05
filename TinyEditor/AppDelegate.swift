816acd5e9d769826a47b82fbb184ede88c676629
krnsoftware <mkarino+github@gmail.com>
2020-08-05 13:50:26 +0900

    first commit.

---
+++ TinyEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TinyEditor
+//
+//  Created by 狩野正嗣 on 2020/08/05.
+//  Copyright © 2020 Masatsugu Karino. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TinyEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TinyEditor
+//
+//  Created by 狩野正嗣 on 2020/08/05.
+//  Copyright © 2020 Masatsugu Karino. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
